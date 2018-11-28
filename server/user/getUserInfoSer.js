926fdf9ebc241f26b73130839943df321c1e4e7f
capoon <13929580503@163.com>
2018-11-28 17:24:52 +0800

    666

--- server/user/getUserInfoSer.js
+++ server/user/getUserInfoSer.js
@@ -18,71 +18,99 @@ function GetUserInfoSer() {
 
     //获取微信企业code的api
     //var getUserCodeUrl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=%s&redirect_uri=%s&response_type=code&scope=%s&state=1#wechat_redirec";
-    var getUserInfoUrl = "https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s";
+    //var getUserInfoUrl = "https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s";
 
 
-    /**
-     * 获取用户的基础数据
-     * @param type
-     * @param code
-     * @param res
-     * @param callback
-     */
-    var getUserInfo = function (type, res, callback) {
-        //拼接获取用户数据的请求链接
-        var acesstoken=serverSerData.instanceMapper[type]['access_token'];
-        var openid=serverSerData.userWxInfo['openid'];
-        var uri = util.format(getUserInfoUrl, acesstoken, openid);
-        //用https方式请求
-        https.get(uri, function (res) {
-            res.setEncoding('utf8');
-            res.on('data', function (data) {
-                var userData = JSON.parse(data);
-                // console.log('get userData1', userData)
-                callback(userData);
-            });
+    // /**
+    //  * 获取用户的基础数据
+    //  * @param type
+    //  * @param code
+    //  * @param res
+    //  * @param callback
+    //  */
+    // var getUserInfo = function (type, res, callback) {
+    //     //拼接获取用户数据的请求链接
+    //     var acesstoken=serverSerData.instanceMapper[type]['access_token'];
+    //     var openid=serverSerData.userWxInfo['openid'];
+    //     var uri = util.format(getUserInfoUrl, acesstoken, openid);
+    //     //用https方式请求
+    //     https.get(uri, function (res) {
+    //         res.setEncoding('utf8');
+    //         res.on('data', function (data) {
+    //             var userData = JSON.parse(data);
+    //             // console.log('get userData1', userData)
+    //             callback(userData);
+    //         });
+    //
+    //     }).on('error', function (e) {
+    //         console.error("Get error: ", e);
+    //         res.send('error');
+    //     });
+    // };
 
-        }).on('error', function (e) {
-            console.error("Get error: ", e);
-            res.send('error');
-        });
-    };
 
-
-    /**
-     * 获取公司员工信息数据
-     * @param req
-     * @param res
-     */
+    // /**
+    //  * 获取公司员工信息数据
+    //  * @param req
+    //  * @param res
+    //  */
+    // this.getWxUserInfo = function (req, res) {
+    //
+    //     //解析url参数
+    //     var type=1;
+    //     var arg = url.parse(req.url, true).query;
+    //     var code = arg['code'];
+    //
+    //     //赋值code
+    //     serverSerData.instanceMapper[type]['code'] = code;
+    //     console.log(code);
+    //
+    //     //获取access_token
+    //     userGeneralSer.getAccessToken(type, res, function () {
+    //
+    //         //获取用户user数据
+    //         console.log(serverSerData.instanceMapper[type]['access_token']);
+    //         console.log(serverSerData.userWxInfo['openid']);
+    //         getUserInfo(type, res, function (userData) {
+    //             if(serverGeneralSer.checkDataNotEmpty(userData)){
+    //                 // console.log('user data',userData);
+    //                 res.send(userData);
+    //             }
+    //             else{
+    //                 //获取用户失败
+    //                 res.send('400');
+    //             }
+    //         })
+    //     })
+    // };
     this.getWxUserInfo = function (req, res) {
 
-        //解析url参数
         var type=1;
         var arg = url.parse(req.url, true).query;
         var code = arg['code'];
+        var appid = serverSerData.instanceMapper[type]['appid'];
+        var secret = serverSerData.instanceMapper[type]['secret'];
 
-        //赋值code
-        serverSerData.instanceMapper[type]['code'] = code;
-        console.log(code);
+        var getAccessTokenUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code";
 
-        //获取access_token
-        userGeneralSer.getAccessToken(type, res, function () {
+        var uri = util.format(getAccessTokenUrl, appid, secret,code);
+        //用request方式请求
+        request.get(uri, function (err, resData, body) {
+            if (!err && resData['statusCode'] == 200) {
+                var userinfo= JSON.parse(body);
+                console.log(userinfo);
+                res.send(userinfo);
 
-            //获取用户user数据
-            console.log(serverSerData.instanceMapper[type]['access_token']);
-            console.log(serverSerData.userWxInfo['openid']);
-            getUserInfo(type, res, function (userData) {
-                if(serverGeneralSer.checkDataNotEmpty(userData)){
-                    // console.log('user data',userData);
-                    res.send(userData);
+            } else {
+                //调用出错
+                console.error("getAccessToken error: ", err);
+                if (res != null) {
+                    res.send('error');
                 }
-                else{
-                    //获取用户失败
-                    res.send('400');
-                }
-            })
-        })
-    };
+            }
+        });
+    }
+
 
 }
 
